ac629b96a133e6fed8b9db0ca951ac03c6af0d95
DESKTOP-GMDTPNR\A-16 <DESKTOP-GMDTPNR\A-16>
2022-08-10 16:03:25 +0800

    Fix gameover not triggering

--- Assets/scripts/TapController.cs
+++ Assets/scripts/TapController.cs
@@ -83,8 +83,7 @@ public class TapController : MonoBehaviour {
 			scoreAudio.Play();
 		}
 
-		if (col.gameObject.tag == "DeadZones") {
-		
+		if (col.gameObject.tag == "DeadZone") {
 			rigidbody.simulated = false;
 			OnPlayerDied ();
 			dieAudio.Play();
